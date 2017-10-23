b68ab838d4a880539e8a3fe6445e68000ec48edb
Krit Sriyanuwatkul <krit2531@hotmail.com>
2017-10-23 13:00:25 +0700

    Fix format

--- 00/0041_circle-packing.c
+++ 00/0041_circle-packing.c
@@ -5,15 +5,13 @@ int main()
 {
   int n;
   scanf("%d", &n);
-    double r = n & 14;
-    if (n == 1)
-    {
-      r += 2;
-    } else if (n == 3){
-      r += sqrt(3);
-    }
-    else if (n & 1)
-      r += -2 + 2 * sqrt(3);
-    printf("%0.6lf\n", r);
+  double r = n & 14;
+  if (n == 1) {
+    r += 2;
+  } else if (n == 3) {
+    r += sqrt(3);
+  } else if (n & 1)
+    r += -2 + 2 * sqrt(3);
+  printf("%0.6lf\n", r);
   return 0;
 }
