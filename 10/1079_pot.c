b90b41113b7c3a7926e3eee9411cc8f092fb4556
Krit Sriyanuwatkul <krit@tdet.co.th>
2017-11-27 11:45:47 +0700

    Add solution

---
+++ 10/1079_pot.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+  int n, k, i = 1, s = 1, j = 0;
+  scanf("%d %d", &n, &k);
+  int a[n];
+  a[0] = 1;
+  while (i < n && i < k) {
+    a[i] = (a[i-1]*2) % 2009;
+    s = (s+a[i])%2009;
+    i++;
+  }
+  if (i < n) {
+    a[i] = s;
+  }
+  while (++i < n) {
+    a[i] = (2*a[i-1]-a[j++]+2009)%2009;
+  }
+  printf("%d\n", a[n-1]);
+  return 0;
+}
