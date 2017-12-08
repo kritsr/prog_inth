479e62ecfc6b20084a315d8007b3fd7e09e8f429
Krit Sriyanuwatkul <krit2531@hotmail.com>
2017-12-09 06:12:10 +0700

    Finish solution

--- 11/1157_electricity.cpp
+++ 11/1157_electricity.cpp
@@ -9,17 +9,17 @@ int main(){
   for (int i = 0; i < N; i++) {
     cin >> p[i];
   }
-  deque<int> q
+  deque<int> q;
   for (int i = 1; i <= K; i++) {
     p[i] += p[0];
+    while (!q.empty() && q.back() > p[i]) q.pop_back();
+    q.push_back(p[i]);
   }
-  sort
-  for (int i = K+1; i < N; i++) {
-    int min = p[i-K];
-    for (int j = i-K+1; j < i; j++) {
-      if (min > p[j]) min = p[j];
-    }
-    p[i] += min;
+  for (int i = K+1, j = 1; i < N; i++, j++) {
+    p[i] += q.front();
+    if (q.front() == p[j]) q.pop_front();
+    while (!q.empty() && q.back() > p[i]) q.pop_back();
+    q.push_back(p[i]);
   }
   cout << p[N-1] << endl;
 }
